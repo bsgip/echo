a6de045b47f512c74d90b07772fd75b42c696fa2
rkravis <ruth.kravis@gmail.com>
2022-02-16 12:12:57 +1100

    updated to reflect new objective construction

--- echo_optimiser.py
+++ echo_optimiser.py
@@ -37,6 +37,7 @@ class EchoOptimiser(object):
         self.model.interval_duration = self.interval_duration
         self.model.number_of_intervals = self.number_of_intervals
         self.model.path_obj = self.ES.path_obj
+        self.model.ES = self.ES
 
         #### Bias Values ####
 
@@ -81,100 +82,8 @@ class EchoOptimiser(object):
             path_obj.initialise_path(self.model)
 
     def apply_constraints(self):
-        self.apply_contingency_constraints()
         self.apply_node_constraints()
         self.apply_path_constraints()
-
-    def apply_contingency_constraints(self):
-
-        def get_port_on_path(node1, node2):
-            """ Gets port on node1 that forms edge connecting node1 and node2 """
-            connecting_edge = self.ES.edge_obj.get((node1.uid, node2.uid))
-            if connecting_edge:
-                return connecting_edge.vertices[0]
-            else:
-                connecting_edge = self.ES.edge_obj.get((node2.uid, node1.uid))
-                if connecting_edge:
-                    return connecting_edge.vertices[1]
-
-        def contingency_power_limited_by_flow_constraints(model, node1, node2, var, flow_constraint):
-            def constraint(model, p, t):
-                a = 0
-                for _, other_path in model.path_obj.items():  # Check if the path includes [...node1, node2...]
-                    if (node1 in other_path.vertices) and (node2 in other_path.vertices):
-                        b = other_path.vertices.index(node1)
-                        c = other_path.vertices.index(node2)
-                        if b + 1 == c:
-                            a += getattr(model, other_path.flow_value)[p, t]
-                return getattr(model, var)[p, t] <= (flow_constraint - a)
-            return constraint
-
-        for _, path_obj in self.ES.path_obj.items():
-            if path_obj.fcas_raise:
-                path_obj.contingency_raise = 'contingency_raise_' + path_obj.path_name
-                setattr(self.model, path_obj.contingency_raise,
-                        en.Var(self.model.Expansion, self.model.Time, initialize=0, domain=en.NonNegativeReals))
-
-                # Iterate through vertices on path to pick up any port constraints along path
-                for i in range(0, len(path_obj.vertices)-1):
-                    node1 = path_obj.vertices[i]
-                    node2 = path_obj.vertices[i+1]
-                    exporting_port = get_port_on_path(node1, node2)
-                    importing_port = get_port_on_path(node2, node1)
-
-                    if exporting_port.export_constraint_value is not None:
-                        con_rule = contingency_power_limited_by_flow_constraints(self.model, node1, node2, path_obj.contingency_raise,                                                       exporting_port.export_constraint_value*-1)
-                        setattr(self.model, f"cont_raise_con_one_{exporting_port.port_name}",
-                                en.Constraint(self.model.Expansion, self.model.Time, rule=con_rule))
-                    if importing_port.import_constraint_value is not None:
-                        con_rule = contingency_power_limited_by_flow_constraints(self.model, node1, node2, path_obj.contingency_raise,
-                                                      importing_port.import_constraint_value)
-                        setattr(self.model, f"cont_raise_con_one_{importing_port.port_name}",
-                                en.Constraint(self.model.Expansion, self.model.Time, rule=con_rule))
-
-                # Meet SOC constraint on contingency providing asset, if applicable
-                if hasattr(path_obj.start_port, 'soc_value'):
-                    def contingency_energy_limited_soc(model, p, t):
-                        return getattr(model, path_obj.contingency_raise)[p, t] * path_obj.fcas_duration / 60 <= \
-                               getattr(model, path_obj.start_port.soc_value)[p, t]
-
-                    setattr(self.model, f"cont_raise_soc_lim_{path_obj.path_name}",
-                            en.Constraint(self.model.Expansion, self.model.Time, rule=contingency_energy_limited_soc))
-
-            if path_obj.fcas_lower:
-                path_obj.contingency_lower = 'contingency_lower_' + path_obj.path_name
-                setattr(self.model, path_obj.contingency_lower,
-                        en.Var(self.model.Expansion, self.model.Time, initialize=0, domain=en.NonNegativeReals))
-
-                # Same constraints apply as fcas raise, except we use the reverse path for collecting port constraints
-                reverse_path = self.ES.path_obj[tuple(path_obj.vertices[::-1])]
-
-                for i in range(0, len(reverse_path.vertices)-1):
-                    node1 = reverse_path.vertices[i]
-                    node2 = reverse_path.vertices[i+1]
-                    exporting_port = get_port_on_path(node1, node2)
-                    importing_port = get_port_on_path(node2, node1)
-
-                    if exporting_port.export_constraint_value is not None:
-                        con_rule = contingency_power_limited_by_flow_constraints(self.model, node1, node2, path_obj.contingency_lower,
-                                                      exporting_port.export_constraint_value*-1)
-                        setattr(self.model, f"cont_lower_con_one_{exporting_port.port_name}",
-                                en.Constraint(self.model.Expansion, self.model.Time, rule=con_rule))
-                    if importing_port.import_constraint_value is not None:
-                        con_rule = contingency_power_limited_by_flow_constraints(self.model, node1, node2, path_obj.contingency_lower,
-                                                      importing_port.import_constraint_value)
-                        setattr(self.model, f"cont_lower_con_one_{importing_port.port_name}",
-                                en.Constraint(self.model.Expansion, self.model.Time, rule=con_rule))
-
-                # Meet SOC constraint on contingency providing asset, if applicable
-                # Todo this won't work if we are also optimising the asset capacity
-                if hasattr(path_obj.start_port, 'soc_value'):
-                    def contingency_energy_limited_soc(model, p, t):
-                        return getattr(model, path_obj.contingency_lower)[p, t] * path_obj.fcas_duration / 60 <= \
-                               path_obj.start_port.max_capacity - getattr(model, path_obj.start_port.soc_value)[p, t]
-
-                    setattr(self.model, f"cont_lower_soc_lim_{path_obj.path_name}",
-                            en.Constraint(self.model.Expansion, self.model.Time, rule=contingency_energy_limited_soc))
 
     def apply_node_constraints(self):
 
@@ -284,22 +193,14 @@ class EchoOptimiser(object):
                         en.Constraint(self.model.Expansion, self.model.Time, rule=no_flow_through_rule))
 
     def build_objective(self):
-        self.objective = 0
-
-        # Walk through graph objects and pick up objectives as defined on nodes/ports/edges/paths
-        for _, node_obj in self.ES.node_obj.items():
-            for _, port_obj in node_obj.ports.items():
-                self.objective += port_obj.add_objective(self.model)
-
-        for _, edge_obj in self.ES.edge_obj.items():
-            self.objective += edge_obj.add_objective(self.model)
-
-        for _, path_obj in self.ES.path_obj.items():
-            self.objective += path_obj.add_objective(self.model)
+        self.model.objective = 0
+        if hasattr(self.ES, 'objective_set'):
+            self.ES.objective_set.initialise_objective(self.model)
+            self.ES.objective_set.set_objective(self.model)
 
     def optimise(self):
         def objective_function(model):
-            return self.objective
+            return self.model.objective
 
         self.model.total_cost = en.Objective(rule=objective_function, sense=en.minimize)
 
