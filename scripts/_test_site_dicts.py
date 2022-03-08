0f6dac8c5da5a1a8305a7b3b2ad58adc43352bef
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2022-03-08 15:11:11 +1100

    minor changes to test dictionary script

--- scripts/_test_site_dicts.py
+++ scripts/_test_site_dicts.py
@@ -31,13 +31,15 @@ plt.hist(num_evs, bins=30)
 
 ## DEFINE SOME DUMMY VALUES TO ASSIGN TO SITES
 ## define load to be used in testing
+rep = 7
+
 test_load = np.array(
     [2.13, 2.09, 2.3, 2.11, 2.2, 2.23, 2.2, 2.15, 2.02, 2.19, 2.19, 2.19, 2.12, 2.15, 2.25, 2.12, 2.21, 2.16,
      2.26, 2.13, 2.08, 2.15, 2.42, 2.02, 2.3, 2.26, 2.35, 2.55, 3.23, 2.98, 3.49, 3.5, 3.12, 3.52, 3.94, 3.55,
      3.99, 3.71, 3.38, 3.76, 3.71, 3.78, 3.29, 3.65, 3.61, 3.75, 3.38, 3.66, 3.56, 3.69, 3.3, 3.61, 3.71, 3.82,
      3.17, 3.69, 3.74, 3.86, 3.57, 3.55, 3.75, 3.6, 3.67, 3.48, 3.51, 3.46, 3.19, 3.38, 3.19, 3.38, 3.04, 3.12,
      2.91, 3.11, 3.13, 2.77, 2.24, 2.54, 2.24, 2.24, 2.09, 2.33, 2.17, 2.16, 1.97, 2.16, 2.21, 2.18, 2.01, 2.16,
-     2.19, 2.11, 2.17, 2.13, 2.05, 2.19] * 7)
+     2.19, 2.11, 2.17, 2.13, 2.05, 2.19] * rep)
 
 # test pv generation values
 test_pv = np.array(
@@ -45,7 +47,7 @@ test_pv = np.array(
      0.74, 0.71, 0.63, 0.68, 0.97, 0.01, 0.52, 0.83, 0.83, 0.79, 1.22, 1.36, 1.27, 1.42, 1.97, 2.56, 2.91, 3.24,
      3.8, 4.3, 4.62, 4.84, 4.6, 4.17, 3.77, 3.76, 3.38, 2.64, 1.96, 1.76, 1.85, 2.4, 3.82, 5.13, 4.97, 5.02, 5.43,
      5.32, 3.56, 1.75, 1.43, 1.65, 1.69, 2.3, 2.71, 2.41, 2.63, 2.6, 1.9, 0.78, 0.13, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] * 7)
+     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] * rep)
 test_pv *= -1  # convert solar generation to negative to match convention.
 
 # define battery
@@ -54,24 +56,24 @@ battery = {'max_capacity': 15., 'depth_of_discharge_limit': 0,
            'charging_efficiency': 1., 'discharging_efficiency': 1.,
            'initial_state_of_charge': 0}
 
-available1 = np.array(([1] * 24 + [0] * 24 + [1] * 24 + [0] * 24) * 7)  # binary for when its available to charge
-usage1 = np.array(([0.0] * 24 + [0.5] * 24 + [0.0] * 24 + [1.0] * 24) * 7)  # energy usage on trip at each time period
+available1 = np.array(([1] * 24 + [0] * 24 + [1] * 24 + [0] * 24) * rep)  # binary for when its available to charge
+usage1 = np.array(([0.0] * 24 + [0.5] * 24 + [0.0] * 24 + [1.0] * 24) * rep)  # energy usage on trip at each time period
 # first vehicle is V2G
 ev1 = {'name': 'ev1', 'available': available1, 'usage': usage1, 'max_capacity': 40., 'depth_of_discharge_limit': 0,
        'charging_power_limit': 10., 'discharging_power_limit': -10, 'charging_efficiency': 1,
        'discharging_efficiency': 1, 'initial_state_of_charge': 0.0}
 
 # Create vehicle 2
-available2 = np.array(([1] * 10 + [0] * 10 + [1] * 28 + [0] * 48) * 7)
-usage2 = np.array(([0.0] * 10 + [0.4] * 10 + [0.0] * 28 + [0.5] * 48) * 7)
+available2 = np.array(([1] * 10 + [0] * 10 + [1] * 28 + [0] * 48) * rep)
+usage2 = np.array(([0.0] * 10 + [0.4] * 10 + [0.0] * 28 + [0.5] * 48) * rep)
 
 # second vehicle is V1G
 ev2 = {'name': 'ev2', 'available': available2, 'usage': usage2, 'max_capacity': 40., 'depth_of_discharge_limit': 0,
        'charging_power_limit': 10., 'discharging_power_limit': 0, 'charging_efficiency': 1,
        'discharging_efficiency': 1, 'initial_state_of_charge': 0.0}
 
-import_tariff_array = np.array(([0.1] * 28 + [0.3] * 8 + [0.2] * 32 + [0.3] * 16 + [0.1] * 12) * 7)
-export_tariff_array = np.array(([0.1] * 96) * 7)
+import_tariff_array = np.array(([0.1] * 28 + [0.3] * 8 + [0.2] * 32 + [0.3] * 16 + [0.1] * 12) * rep)
+export_tariff_array = np.array(([0.1] * 96) * rep)
 
 # test_site_3_dict = {'name': 'test_site_3', 'load_profile': test_load,
 #                     'pv_profile': test_pv, 'battery': battery,
@@ -115,7 +117,7 @@ for i, r in con_point_df.iterrows():
 scenario = {'name':'test_scenario', 'network':'ausnet_a',
             'num_sites':num_sites, 'ev_percent':ev_percent,
             'battery_percent':bat_percent, 'pv_percent':pv_percent,
-            'sites':sites, 'interval_duration':15, 'time_periods':4*24*7}
+            'sites':sites, 'interval_duration':15, 'time_periods':4*24*rep}
 
 # test saving and loading as a dict
 t1 = time.time()
