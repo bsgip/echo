b452d41e2353304f488568e0334695881567d0fb
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2022-04-07 11:21:05 +1000

    updates version number, adds more comments to ev and btm example

--- scripts/ev_example.py
+++ scripts/ev_example.py
@@ -16,6 +16,11 @@ from echo.configuration import *
 from echo.objectives import *
 from networkx import Graph, draw
 
+
+"""
+        Example of optimising the charging of two evs, where there is also a load at the site
+"""
+
 # set up seaborn the way you like
 sns.set_style({'axes.linewidth': 1, 'axes.edgecolor': 'black', 'xtick.direction': \
     'out', 'xtick.major.size': 4.0, 'ytick.direction': 'out', 'ytick.major.size': 4.0, \
@@ -24,6 +29,7 @@ sns.set_style({'axes.linewidth': 1, 'axes.edgecolor': 'black', 'xtick.direction'
 ############################ Define an Example Optimisation Problem ########################################
 
 # The load and pv arrays below are in average kw consumed per 15 minutes
+# define load (loads must be positive values)
 test_load = np.array(
     [2.13, 2.09, 2.3, 2.11, 2.2, 2.23, 2.2, 2.15, 2.02, 2.19, 2.19, 2.19, 2.12, 2.15, 2.25, 2.12, 2.21, 2.16,
      2.26, 2.13, 2.08, 2.15, 2.42, 2.02, 2.3, 2.26, 2.35, 2.55, 3.23, 2.98, 3.49, 3.5, 3.12, 3.52, 3.94, 3.55,
@@ -32,13 +38,6 @@ test_load = np.array(
      2.91, 3.11, 3.13, 2.77, 2.24, 2.54, 2.24, 2.24, 2.09, 2.33, 2.17, 2.16, 1.97, 2.16, 2.21, 2.18, 2.01, 2.16,
      2.19, 2.11, 2.17, 2.13, 2.05, 2.19])
 
-test_pv = 2 * np.array(
-    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05, 0.23, 0.52,
-     0.74, 0.71, 0.63, 0.68, 0.97, 0.01, 0.52, 0.83, 0.83, 0.79, 1.22, 1.36, 1.27, 1.42, 1.97, 2.56, 2.91, 3.24,
-     3.8, 4.3, 4.62, 4.84, 4.6, 4.17, 3.77, 3.76, 3.38, 2.64, 1.96, 1.76, 1.85, 2.4, 3.82, 5.13, 4.97, 5.02, 5.43,
-     5.32, 3.56, 1.75, 1.43, 1.65, 1.69, 2.3, 2.71, 2.41, 2.63, 2.6, 1.9, 0.78, 0.13, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-test_pv *= -1  # convert solar generation to negative to match convention.
 
 # Tariffs are in $ / kwh
 import_tariff_array = np.array(([0.1] * 28 + [0.3] * 8 + [0.2] * 32 + [0.3] * 16 + [0.1] * 12))
@@ -49,61 +48,75 @@ export_tariff_array = np.array(([0.0] * 96))
 np.set_printoptions(suppress=True)
 
 ## Set up hyper params
-time_periods = len(test_load)
-interval_duration = 15
-expansion_periods = 1
-discount_rate = 0
+time_periods = len(test_load)   # number of time periods to run the optimisation for
+interval_duration = 15          # each time period is 15 mins long
+expansion_periods = 1           # not yet implemented leave as 1
+discount_rate = 0               # not yet implemented leave as 0
 
 # Create graph
 system = OptimisationGraph()
 
 # Create assets
-grid = Node()
-grid.add_named_electrical_ports(['grid'])
+grid = Node()                                   # create node representing upstream grid
+grid.add_named_electrical_ports(['grid'])       # create a port which will be used to connect this with the connection_point
 
+# create the connection point (where we will sum everything up)
 connection_point = ElectricalTellegenNode()
-connection_point.add_named_electrical_ports(['load', 'ev1', 'ev2', 'grid'])
+connection_point.add_named_electrical_ports(['load', 'ev1', 'ev2', 'grid'])  # create ports to connect to the grid, the load, and the inverter
+# set constraints on how power can flow from connection point to the evs
+# i.e. export here would be exporting from grid to the ev battery
+# and import would be from the ev to the grid
+# hence this is possibly the opposite of what you might think. A more intuitive
+# place for constraints on ev charging is found later
 connection_point.ports['ev1'].set_flow_constraints(max_import=7.5, max_export=-7.5)
-connection_point.ports['ev2'].set_flow_constraints(max_import=7.5, max_export=-7.5)
+connection_point.ports['ev2'].set_flow_constraints(max_import=7.5, max_export=-5.)
 
-load = Node()
-l1 = ElectricalDemand()
+load = Node()                       # create a node to represent the load
+l1 = ElectricalDemand()             # create an electrical demand to attach to this node
 l1.add_demand_profile_from_array(test_load, expansion_periods)
-load.ports['load'] = l1
+load.ports['load'] = l1             # add the electrical demand to a port of the load node
+
+# create the evs
+# each ev consists of three nodes, a connecting node, a usage (electrical demand),
+# and a storage node.
 
 # Create vehicle 1
 available1 = np.array([1] * 24 + [0] * 24 + [1] * 24 + [0] * 24)    # bool when at charger
 usage1 = np.array([0.0]*24 + [0.5]*24 + [0.0]*24 + [1.0]*24)        # kw average during use
 
-ev1_cp = ElectricalTellegenNode()
-ev1_cp.add_named_electrical_ports(['cp', 'ev', 'usage'])
+ev1_cp = ElectricalTellegenNode()       # create teh connecting point
+ev1_cp.add_named_electrical_ports(['cp', 'ev', 'usage'])    # create the ports required
+# allow the connecting point to only be actively conencted to the grid when the vehicle is available
 ev1_cp.ports['cp'].add_active_periods_from_array(available1, expansion_periods)
 
-ev1 = ElectricalStorage(max_capacity=40.0,
-                        depth_of_discharge_limit=0,
-                        charging_power_limit=10,
-                        discharging_power_limit=-10,
-                        charging_efficiency=1,
-                        discharging_efficiency=1,
-                        initial_state_of_charge=10)
+# create the electrical storage
+ev1 = ElectricalStorage(max_capacity=40.0,              # max capacity of battery in kwh
+                        depth_of_discharge_limit=0,     # allowable depth of discharge in range [0,100] (i.e. percent)
+                        charging_power_limit=10,        # max charging rate in kW
+                        discharging_power_limit=-10,    # max discharging rate in kW
+                        charging_efficiency=1,          # charging efficiency in range [0,1]
+                        discharging_efficiency=1,       # discharging efficiency in range [0,1]
+                        initial_state_of_charge=10)     # initial state of charge in kWh
 
+# create a node to connect this storage to
 vehicle1 = Node()
 # hacky implemetnation of ev user conservativeness while plugged in
 vehicle1.ports['ev'] = ev1
 # next few setting are to replicate a conservative user who wants to keep a mininum SOC while plugged in
-vehicle1.ports['ev'].soc_conserv = 20.  # kWh
-vehicle1.ports['ev'].soc_conserv_cost = 100.
-vehicle1.ports['ev'].available = available1
+vehicle1.ports['ev'].soc_conserv = 20.  # set a value that the user wants the battery to stay above while plugged in (kWh)
+vehicle1.ports['ev'].soc_conserv_cost = 100.    # set the cost above which the user would be willing to let it go below this
+vehicle1.ports['ev'].available = available1     # set the availability for the conservative value (i.e. needs to know when plugged in)
 # next setting is to deal with infeasible trip data
 vehicle1.ports['ev'].enable_trip_slack = False  # make true to allow infeasible trips to be public charged
 
 
-
+# create a node for the usage
 trip1 = Node()
-us1 = ElectricalDemand()
+us1 = ElectricalDemand()    # create an electrical demand object for this
 us1.add_demand_profile_from_array(usage1, expansion_periods=expansion_periods)
 trip1.ports['usage'] = us1
 
+# craete a second vehicle (same options)
 # Create vehicle 2
 available2 = np.array([1]*10 + [0]*10 + [1]*28 + [0]*48)
 usage2 = np.array([0.0]*10 + [0.4]*10 + [0.0]*28 + [0.5]*48)
@@ -145,10 +158,12 @@ system.connect_ports_and_create_edge(grid.ports['grid'], connection_point.ports[
 system.connect_ports_and_create_edge(connection_point.ports['load'], load.ports['load'])
 
 # Create objectives/tariffs
+# create an import tariff
 import_tariff = ImportTariff(component=connection_point.ports['grid'],
                              tariff_array=import_tariff_array,
                              expansion_periods=expansion_periods)
 
+# add a throughput cost to the ev battery
 throughput_cost = ThroughputCost(component=ev1, rate=0.000001)
 
 objective_set = ObjectiveSet(objective_list=[throughput_cost, import_tariff])
@@ -180,10 +195,9 @@ hrs = np.arange(0, len(test_load)) / 4
 fig = plt.figure(figsize=(14, 7))
 ax1 = fig.add_subplot(4, 1, 1)
 line1, = ax1.plot(hrs, test_load, color=colors[0])
-line2, = ax1.plot(hrs, test_pv, color=colors[1])
 line3, = ax1.plot(hrs, optimised_connection_point_load, color=colors[2])
 ax1.set_xlabel('hour'), ax1.set_ylabel('kW')
-ax1.legend([line1, line2, line3], ['Load', 'PV', 'Connection Point'], ncol=3)
+ax1.legend([line1, line3], ['Load', 'Connection Point'], ncol=3)
 ax1.set_xlim([0, len(test_load) / 4])
 
 ax2 = fig.add_subplot(4, 1, 2)
