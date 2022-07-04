0fa25565d4ddf37cb3b48da45e92c586a209054d
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2022-07-04 10:43:44 +1000

    updates version number

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if sys.version_info < (3, 7):
 
 setuptools.setup(
     name="echo",
-    version="1.0.4",
+    version="1.0.5",
     description="For ",
     long_description=long_description,
     long_description_content_type="text/markdown",
