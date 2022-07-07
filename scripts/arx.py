6f3525bb28bacc4baa29b0e646f064c93915ee05
rkravis <ruth.kravis@gmail.com>
2022-07-07 16:20:05 +1000

    minor

--- scripts/arx.py
+++ scripts/arx.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -13,8 +12,8 @@ df = pd.read_csv('../bz_data/thermal_load_arx_b155_data.csv')
 
 # mse_test, mse_trained, model_coef = train_arx_on_data(u=df['energy_in'], y=df['avg_temp'], na=1, nb=1, training_test_split=67)
 
-mse_test, mse_trained, model_coef = train_arx_multiple_inputs(u=df[['energy_in', 'temp_out', 'temp_sp', 'bld_hrs']],
+mse_test, mse_trained, model_coef = train_arx_multiple_inputs(u=df[['energy_in', 'temp_out', 'temp_sp']],
                                                               y=df['avg_temp'],
-                                                              na=2,
-                                                              n_inputs=[2, 2, 0, 0],
+                                                              na=1,
+                                                              n_inputs=[1, 1, 1],
                                                               training_test_split=67)
