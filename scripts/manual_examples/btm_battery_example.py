a74d1df9f6f3a1182dba8f3658fffe4266b22846
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2022-10-14 14:17:15 +1100

    more timing

--- scripts/manual_examples/btm_battery_example.py
+++ scripts/manual_examples/btm_battery_example.py
@@ -26,6 +26,8 @@ sns.set_style({'axes.linewidth': 1, 'axes.edgecolor': 'black', 'xtick.direction'
 
 ############################ Define an Example Optimisation Problem ########################################
 
+duration_multiplication = 10
+
 # The load and pv arrays below are in average kw consumed per 15 minutes
 # define load (loads must be positive values)
 test_load = np.array(
@@ -34,7 +36,7 @@ test_load = np.array(
      3.99, 3.71, 3.38, 3.76, 3.71, 3.78, 3.29, 3.65, 3.61, 3.75, 3.38, 3.66, 3.56, 3.69, 3.3, 3.61, 3.71, 3.82,
      3.17, 3.69, 3.74, 3.86, 3.57, 3.55, 3.75, 3.6, 3.67, 3.48, 3.51, 3.46, 3.19, 3.38, 3.19, 3.38, 3.04, 3.12,
      2.91, 3.11, 3.13, 2.77, 2.24, 2.54, 2.24, 2.24, 2.09, 2.33, 2.17, 2.16, 1.97, 2.16, 2.21, 2.18, 2.01, 2.16,
-     2.19, 2.11, 2.17, 2.13, 12, 12])
+     2.19, 2.11, 2.17, 2.13, 12, 12] * duration_multiplication)
 
 # define PV, generation is negative values
 test_pv = 2 * np.array(
@@ -42,12 +44,12 @@ test_pv = 2 * np.array(
      0.74, 0.71, 0.63, 0.68, 0.97, 0.01, 0.52, 0.83, 0.83, 0.79, 1.22, 1.36, 1.27, 1.42, 1.97, 2.56, 2.91, 3.24,
      3.8, 4.3, 4.62, 4.84, 4.6, 4.17, 3.77, 3.76, 3.38, 2.64, 1.96, 1.76, 1.85, 2.4, 3.82, 5.13, 4.97, 5.02, 5.43,
      5.32, 3.56, 1.75, 1.43, 1.65, 1.69, 2.3, 2.71, 2.41, 2.63, 2.6, 1.9, 0.78, 0.13, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] * duration_multiplication)
 test_pv *= -1  # convert solar generation to negative to match convention.
 
 # Tariffs are in $ / kwh
-import_tariff_array = np.array(([0.1] * 28 + [0.3] * 8 + [0.2] * 32 + [0.3] * 16 + [0.1] * 12))
-export_tariff_array = np.array(([0.0] * 96))
+import_tariff_array = np.array(([0.1] * 28 + [0.3] * 8 + [0.2] * 32 + [0.3] * 16 + [0.1] * 12) * duration_multiplication)
+export_tariff_array = np.array(([0.0] * 96) * duration_multiplication)
 
 ############################ Optimise this Example ########################################
 
