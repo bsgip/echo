d879f675f6db73078a01815c32bab8760fd28248
annaskobeleva <anna.skobeleva@anu.edu.au>
2022-10-21 11:18:59 +1100

    Acton model prototype

--- scripts/builder_examples/new_network_builder_example.py
+++ scripts/builder_examples/new_network_builder_example.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import time as time
 from pprint import pprint
-from echo.configuration import *
+import echo.configuration as ec
 
 # Define our time periods and interval duration
 time_periods = 48
@@ -34,7 +34,7 @@ inverter_params = {'ac_port_name': 'cp',
 
 solar_params = {'curtailable': True}
 
-ev_params = {'charge_mode': EVChargeMode.V2G,
+ev_params = {'charge_mode': ec.EVChargeMode.V2G.value,
              'available': 'ev_available',  # we can pass a reference col name instead of data
              'usage': 'ev_usage',  # we can pass a reference col name instead of data
              'max_capacity': 40.,
@@ -57,45 +57,45 @@ n.add_profile(profile=df)
 
 # Add all our components (nodes)
 
-n.add_node_to_components(n_id='grid', n_type=NodeType.FlexWithEmissions, ports=['downstream', 'co2'],
+n.add_node_to_components(n_id='grid', n_type=ec.NodeType.FlexWithEmissions, ports=['downstream', 'co2'],
                          params={'emitting_port': 'downstream',
                                  'carbon_port': 'co2',
                                  'emissions_factor': 60})
 
-n.add_node_to_components(n_id='emissions', n_type=NodeType.CarbonAggregation, ports=['grid'])
-n.add_node_to_components(n_id='cp', n_type=NodeType.ElectricalTellegen, ports=['upstream', 'load', 'inv', 'ev'])
-n.add_node_to_components(n_id='inverter', n_type=NodeType.Inverter, ports=['cp', 'bess', 'pv'], params=inverter_params)
-n.add_node_to_components(n_id='battery', n_type=NodeType.Battery, ports=['bess'], params=battery_params)
-n.add_node_to_components(n_id='solar', n_type=NodeType.Solar, ports=['pv'], data='solar', params=solar_params)
-n.add_node_to_components(n_id='ev', n_type=NodeType.EV, ports=['ev_cp'], params=ev_params)
+n.add_node_to_components(n_id='emissions', n_type=ec.NodeType.CarbonAggregation, ports=['grid'])
+n.add_node_to_components(n_id='cp', n_type=ec.NodeType.ElectricalTellegen, ports=['upstream', 'load', 'inv', 'ev'])
+n.add_node_to_components(n_id='inverter', n_type=ec.NodeType.Inverter, ports=['cp', 'bess', 'pv'], params=inverter_params)
+n.add_node_to_components(n_id='battery', n_type=ec.NodeType.Battery, ports=['bess'], params=battery_params)
+n.add_node_to_components(n_id='solar', n_type=ec.NodeType.Solar, ports=['pv'], data='solar', params=solar_params)
+n.add_node_to_components(n_id='ev', n_type=ec.NodeType.EV, ports=['ev_cp'], params=ev_params)
 
 # For nodes with time series data, we pass the column name of the data in the dataframe.
-n.add_node_to_components(n_id='load', n_type=NodeType.ElectricalLoad, ports=['load'], data='load')
+n.add_node_to_components(n_id='load', n_type=ec.NodeType.ElectricalLoad, ports=['load'], data='load')
 
 # Add edges
-n.add_edge_between_ports(node_tuple=('grid', 'cp'), port_tuple=('downstream', 'upstream'), resource=Units.KW)
-n.add_edge_between_ports(node_tuple=('cp', 'load'), port_tuple=('load', 'load'), resource=Units.KW)
-n.add_edge_between_ports(node_tuple=('cp', 'inverter'), port_tuple=('inv', 'cp'), resource=Units.KW)
-n.add_edge_between_ports(node_tuple=('inverter', 'battery'), port_tuple=('bess', 'bess'), resource=Units.KW)
-n.add_edge_between_ports(node_tuple=('inverter', 'solar'), port_tuple=('pv', 'pv'), resource=Units.KW)
-n.add_edge_between_ports(node_tuple=('cp', 'ev'), port_tuple=('ev', 'ev_cp'), resource=Units.KW)
-n.add_edge_between_ports(node_tuple=('grid', 'emissions'), port_tuple=('co2', 'grid'), resource=Units.CO2)
+n.add_edge_between_ports(node_tuple=('grid', 'cp'), port_tuple=('downstream', 'upstream'), resource=ec.Units.KW.value)
+n.add_edge_between_ports(node_tuple=('cp', 'load'), port_tuple=('load', 'load'), resource=ec.Units.KW.value)
+n.add_edge_between_ports(node_tuple=('cp', 'inverter'), port_tuple=('inv', 'cp'), resource=ec.Units.KW.value)
+n.add_edge_between_ports(node_tuple=('inverter', 'battery'), port_tuple=('bess', 'bess'), resource=ec.Units.KW.value)
+n.add_edge_between_ports(node_tuple=('inverter', 'solar'), port_tuple=('pv', 'pv'), resource=ec.Units.KW.value)
+n.add_edge_between_ports(node_tuple=('cp', 'ev'), port_tuple=('ev', 'ev_cp'), resource=ec.Units.KW.value)
+n.add_edge_between_ports(node_tuple=('grid', 'emissions'), port_tuple=('co2', 'grid'), resource=ec.Units.CO2.value)
 
 # Define objectives
 
 n.add_objective(name='import_cost',
                 component={'node': 'cp', 'port': 'upstream'},
-                obj_type=TariffType.ImportTariff,
+                obj_type=ec.TariffType.ImportTariff.value,
                 prices=[0] * (time_periods // 2) + [0] * (time_periods // 2))
 
 n.add_objective(name='carbon_cost',
                 component={'node': 'emissions', 'port': 'grid'},
-                obj_type=TariffType.ImportTariff,
+                obj_type=ec.TariffType.ImportTariff.value,
                 prices=[0] * time_periods)
 
 n.add_objective(name='export_cost',
                 component={'node': 'cp', 'port': 'upstream'},
-                obj_type=TariffType.ExportTariff,
+                obj_type=ec.TariffType.ExportTariff.value,
                 prices=[0] * (time_periods // 2) + [0] * (time_periods // 2))
 
 # Define some demand charges as dicts
@@ -109,7 +109,7 @@ dc2_param = {'name': 'peak',
 
 n.add_objective(name='import_demand_tariff',
                 component={'node': 'cp', 'port': 'upstream'},
-                obj_type=TariffType.ImportDemandTariff,
+                obj_type=ec.TariffType.ImportDemandTariff.value,
                 charges=[dc1_param, dc2_param])
 
 # Validate the network to make sure there is consistency between component naming, edge naming, objective naming.
--- scripts/bz_model/acton_campus_model.py
+++ scripts/bz_model/acton_campus_model.py
@@ -1,5 +1,86 @@
 import pandas as pd
 import numpy as np
-from echo import echo_builder,  configuration, echo_models, echo_optimiser, objectives
+from echo import objectives
+import echo.echo_models as models
+import echo.echo_builder as builder
+import echo.configuration as config
+import echo.echo_optimiser as optimiser
 
+df = pd.DataFrame({'bl1_gas_demand': [0.4, 0.5, 0.6, 1.1, 0.8],
+                   'bl2_gas_demand': [0.7, 0.8, 1.1, 1.3, 0.9],
+                   'bl1_el_demand_net': [7, 8, 10, 8, 11],
+                   'bl2_el_demand_power': [5, 5, 6, 9, 7],
+                   'bl2_el_demand_chiller': [73, 66, 71, 78, 60]})
+
+feeder_rating_kva = 3.18*1e3
+tx_rating_kva = 1000
 time_periods = 10
+
+## Create Electrical Grid and Gas Grid nodes
+gas_grid = models.Node(node_name ='GasGrid',
+                        node_rule = config.NodeRule.Custom,
+                        ports = {'bulk_gas_source': models.FlexSource(port_name='bulk_gas_source', units=config.Units.JPS),
+                                 'bulk_gas_emissions': models.CarbonSource(port_name='bulk_gas_emissions')})
+
+gas_grid = models.FlexNodeWithEmissions(node_name='GasGrid',
+                                        emitting_port='bulk_gas_source',
+                                        emitting_port_units=config.Units.JPS,
+                                        carbon_port='bulk_gas_emissions',
+                                        emissions_factor=50, ## Assuming 1 GJ of natural gas will produce 50 kg of CO2
+                                        )
+gas_distribution = models.TellegenNode(node_name='GasGridTellegen',
+                                       ports={'gas_grid': models.FlexSink(port_name='grid', units=config.Units.JPS),
+                                              'gas_sp01': models.FlexSource(port_name='gas_sp01', units=config.Units.JPS),
+                                              'gas_sp02': models.FlexSource(port_name='gas_sp01', units=config.Units.JPS)}
+                                       )
+
+
+gas_sp01 = models.TellegenNode(node_name='GasSupply01',
+                               ports={'gas_grid': models.FlexSink(port_name='gas_grid', units=config.Units.JPS),
+                                      'bl1': models.FlexSource(port_name='bl1', units=config.Units.JPS)}
+                               )
+
+gas_sp02 = models.TellegenNode(node_name='GasSupply02',
+                               ports={'gas_grid': models.FlexSink(port_name='gas_grid', units=config.Units.JPS),
+                                      'bl2': models.FlexSource(port_name='bl2', units=config.Units.JPS)}
+                               )
+
+
+## Gas grid should only export ?
+# gas_grid.ports['bulk_gas_source'].flows = config.Flows.Export
+
+electrical_grid = models.FlexElectricalNode(port_name='AvenueFeeder')
+carbon_aggregation = models.CarbonAggregation()
+
+
+## MV Feeder Node
+## Set grid port to slack=True
+avenue_feeder = models.TellegenNode(node_name='AvenueFeeder',
+                                    ports={'grid': models.FlexPort(port_name='grid', units=config.Units.KW,
+                                                                   import_constraint=config.FlowConstraint.Fixed,
+                                                                   export_constraint=config.FlowConstraint.Fixed,
+                                                                   import_constraint_value=feeder_rating_kva,
+                                                                   export_constraint_value=-feeder_rating_kva),
+                                           's001': models.FlexPort(port_name='s001', units=config.Units.KW),
+                                           's002': models.FlexPort(port_name='s002', units=config.Units.KW)})
+
+## Substation (distribution transformer Node)
+## Substation has import/export constraints on grid port that equlas its KVA rating
+s001 = models.TellegenNode(node_name='Substation001',
+                           ports={'feeder': models.FlexPort(port_name='feeder', units=config.Units.KW,
+                                                            import_constraint=config.FlowConstraint.Fixed,
+                                                            import_constraint_value=tx_rating_kva,
+                                                            export_constraint=config.FlowConstraint.Fixed,
+                                                            export_constraint_value=-tx_rating_kva),
+                                  'bl1': models.FlexPort(port_name='bl1', units=config.Units.KW),
+                                  'bl2': models.FlexPort(port_name='bl2', units=config.Units.KW)}
+                           )
+
+
+bl1_cp = models.MultiCommodityTellegenNode(node_name='Bld1',
+                                           ports={})
+bl2_cp = models.MultiCommodityTellegenNode()
+
+
+
+system = models.OptimisationGraph()
