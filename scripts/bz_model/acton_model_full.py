40151b7829387586b3e65efe684d5c9285fceb03
annaskobeleva <anna.skobeleva@anu.edu.au>
2022-11-16 13:21:15 +1100

    gas and el topology dicts

--- scripts/bz_model/acton_model_full.py
+++ scripts/bz_model/acton_model_full.py
@@ -12,50 +12,12 @@ import pyomo.environ as en
 
 ########### local imports ###############
 from read_datafiles import bl_with_meters, kambri_central, central_plant, source_df, extra_gas_supply
+from read_datafiles import gas_supply_points_dict, bld_to_model, electrical_feeders, bl_substations, bld_not_included, substations_bl
 from bz_model_constants import *
 from custom_classes import MultiCommodityTellegenNodeManual, MultiCommodityTellegenNode
 from graph_plotting_utils import *
 from tariffs_and_costs import *
 from collections import defaultdict
-
-
-bl_to_model = set(bl_with_meters[bl_with_meters.include_in_model.isin(['Y'])].ID)
-
-gas_supply_points_dict= dict()
-for gas_meter, group in bl_with_meters[~bl_with_meters.gas_meter.isnull()&bl_with_meters.include_in_model.isin(['Y'])].groupby('gas_meter'):
-    gas_supply_points_dict[gas_meter]=list(group.ID)
-
-
-electrical_feeders= defaultdict()
-
-for _f, group in bl_with_meters[~bl_with_meters.Feeder.isnull()].groupby('Feeder'):
-    feeder_subs = []
-    unique_subs = [_s for _s in group[~group.substation.isnull()].substation.unique() if _s!='1225,1744']
-    for _s in unique_subs:
-        feeder_subs.extend(str(_s).split(','))
-    electrical_feeders[_f]=[sub_id for sub_id in unique_subs]
-
-
-electrical_substations=defaultdict()
-
-for _bl, group in bl_with_meters[bl_with_meters.include_in_model.isin(['Y'])].groupby('ID'):
-    bl_subs = []
-    feeder_id = list(group.Feeder.unique())[0]
-    unique_subs = list(group[~group.substation.isnull()].substation.unique())
-    if len(unique_subs)==0:
-        continue
-    for _s in unique_subs:
-        bl_subs.extend(str(_s).split(','))
-    print(f'bl {_bl} fed from subs {bl_subs}')
-
-
-
-
-
-
-
-
-
 
 
 
--- scripts/bz_model/read_datafiles.py
+++ scripts/bz_model/read_datafiles.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from collections import defaultdict
 
 
 
@@ -8,12 +9,9 @@ central_plant = {'135': ['46', '48', '141', '134', '136', '137', '138', '122']}
 # List of buildings with extra gas meters for a domestic gas supply: hot water, cooking, steam generation
 extra_gas_supply = ['46', '141', '134', '136', '137', '138', '15']
 bl_with_meters = pd.read_excel('/home/anna/GitBSGIP/bz_data/processed_data/buildings_with_meters.xlsx', dtype={'gas_meter': str,
-                                                                                                               'ID': str,
+                                                                                                             'ID': str,
                                                                                                                'substation': str})
-
-
-
-
+bld_to_model = set(bl_with_meters[bl_with_meters.include_in_model.isin(['Y'])].ID)
 
 
 heatpump_cop = pd.read_csv('/home/anna/GitBSGIP/bz_data/source_data/heatpump_cop.csv')
@@ -25,5 +23,40 @@ source_df = pd.DataFrame({'bl_gas_demand': [0.4, 0.5, 0.6, 1.1, 0.8],
                    'bl_cooling_demand': [387.4, 437.5, 481.0, 549.1, 569.3]})
 
 source_df = source_df.merge(heatpump_cop, how='left', left_on='ambient_temp', right_on='ambient_temp')
+
+
+## Dictionaries with gas and electrical topologies
+
+gas_supply_points_dict = dict()
+for gas_meter, group in bl_with_meters[
+    ~bl_with_meters.gas_meter.isnull() & bl_with_meters.include_in_model.isin(['Y'])].groupby('gas_meter'):
+    gas_supply_points_dict[gas_meter] = list(group.ID)
+
+electrical_feeders = defaultdict()
+
+for _f, group in bl_with_meters[~bl_with_meters.Feeder.isnull()].groupby('Feeder'):
+    feeder_subs = []
+    unique_subs = [_s for _s in group[~group.substation.isnull()].substation.unique() if _s != '1225,1744']
+    for _s in unique_subs:
+        feeder_subs.extend(str(_s).split(','))
+    electrical_feeders[_f] = [sub_id for sub_id in unique_subs]
+
+bl_substations = defaultdict()
+bld_not_included = []
+for _bl, group in bl_with_meters[bl_with_meters.include_in_model.isin(['Y'])].groupby('ID'):
+    bl_subs = []
+    unique_subs = list(group[~group.substation.isnull()].substation.unique())
+    if len(unique_subs) == 0:
+        bld_not_included.append(_bl)
+        continue
+    for _s in unique_subs:
+        bl_subs.extend(str(_s).split(','))
+    bl_substations[_bl] = bl_subs
+
+substations_bl = defaultdict(list)
+for k,v in bl_substations.items():
+    for _sub in v:
+        substations_bl[_sub].append(k)
+
 
 
