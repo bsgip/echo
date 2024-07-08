af8d26cdf9d64a0fff8c4ab8c86678ba0417ba3d
Mike Turner <mike.turner@anu.edu.au>
2024-07-08 18:48:07 +0800

    Moves inject method into correct location under class

--- src/echo/models/base.py
+++ src/echo/models/base.py
@@ -1194,10 +1194,9 @@ class OptimisationGraph(BaseModel):
         }
         self._update_node(node_name=node_name, **node_attributes)
 
-
-def inject_data_into_heatpump(self, node_name: str, heating_cop_time_series, cooling_cop_time_series):
-    node_attributes = {
-        "heating_cop_time_series": heating_cop_time_series,
-        "cooling_cop_time_series": cooling_cop_time_series,
-    }
-    self._update_node(node_name=node_name, **node_attributes)
+    def inject_data_into_heatpump(self, node_name: str, heating_cop_time_series, cooling_cop_time_series):
+        node_attributes = {
+            "heating_cop_time_series": heating_cop_time_series,
+            "cooling_cop_time_series": cooling_cop_time_series,
+        }
+        self._update_node(node_name=node_name, **node_attributes)
